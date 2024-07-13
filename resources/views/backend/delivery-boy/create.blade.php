5168917c7f1ce3c7ae941c63f15a695eb09970a5
Jay R SG <jaysen110@gmail.com>
2024-07-13 21:50:59 +0600

    default password set for delivery login

--- resources/views/backend/delivery-boy/create.blade.php
+++ resources/views/backend/delivery-boy/create.blade.php
@@ -85,7 +85,7 @@
                                 <h6 class="mb-0">Passowrd</h6>
                             </div>
                             <div class="col-sm-9 text-secondary">
-                                <input type="password" class="form-control" name="password" />
+                                <input type="text" class="form-control" name="password" value="12345678" />
                                 @error('password')
                                     <div style="color: red;">{{ $message }}</div>
                                 @enderror
@@ -96,7 +96,7 @@
                                 <h6 class="mb-0">Confirm Passowrd</h6>
                             </div>
                             <div class="col-sm-9 text-secondary">
-                                <input type="password" class="form-control" name="password_confirmation" />
+                                <input type="text" class="form-control" name="password_confirmation" value="12345678" />
                             </div>
                         </div>
                         <div class="row">
